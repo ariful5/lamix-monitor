5763c446978c41ddbe2c6f4ff8fa7a30d6dde572
ariful5 <66830965+ariful5@users.noreply.github.com>
2026-05-28 09:22:47 +0600

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -9,7 +9,7 @@ import time
 # ─── Config ───────────────────────────────────────────────
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN', '')
 GITHUB_TOKEN   = os.environ.get('MY_PAT_TOKEN', '')
-GITHUB_REPO    = os.environ.get('GITHUB_REPO', 'ariful5/lamix-monitor')
+GITHUB_REPO = os.environ.get('GITHUB_REPO', 'ariful5/my-project-2024')
 CONFIG_FILE    = 'users_config.json'
 ALERT_GROUP_ID = os.environ.get('ALERT_GROUP_ID', '')
 
